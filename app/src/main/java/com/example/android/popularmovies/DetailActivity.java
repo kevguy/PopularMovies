1d2e9e225338c5ac5a394fd8e791e020523609aa
kevguy <kevlai22@uw.edu>
2015-10-09 00:44:11 +0800

    Make a dummy Detail activity (detail for fav hist activity), after surviving the update to 1.4 of Android Studio

--- app/src/main/java/com/example/android/popularmovies/DetailActivity.java
+++ app/src/main/java/com/example/android/popularmovies/DetailActivity.java
@@ -23,14 +23,14 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.support.v7.widget.ShareActionProvider;
 
-        import android.os.Bundle;
-        import android.support.v4.app.Fragment;
-        import android.support.v7.app.ActionBarActivity;
-        import android.view.LayoutInflater;
-        import android.view.Menu;
-        import android.view.MenuItem;
-        import android.view.View;
-        import android.view.ViewGroup;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import java.util.ArrayList;
